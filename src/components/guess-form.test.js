57d95409778b53892a5503e3de8b760e4de6522a
Serena Lertkantitham <serena.lertkantitham@gmail.com>
2019-04-20 09:07:43 -0700

    guess-form.test.js done

---
+++ src/components/guess-form.test.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { shallow, mount, configure } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+
+configure({ adapter: new Adapter() });
+
+import { GuessForm } from "./guess-form";
+import { makeGuess } from "../actions";
+
+describe("GuessForm", () => {
+  it("Dispatches makeGuess when the form is submit", () => {
+    const dispatch = jest.fn();
+    const wrapper = mount(<GuessForm dispatch={dispatch} />);
+    const value = "3";
+    wrapper.find('input[type="number"]').instance().value = value;
+    wrapper.simulate("submit");
+    expect(dispatch).toHaveBeenCalledWith(makeGuess(value));
+  });
+});
